85bc8d05b252a21ab03c9ec36b42c472ce7f54b3
khoa-24bit <2251120218@ut.edu.vn>
2025-08-03 22:42:45 +0700

    Create BE WM project

---
+++ src/main/java/org/wp/wpproject/WpProjectApplication.java
@@ -0,0 +1,13 @@
+package org.wp.wpproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WpProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WpProjectApplication.class, args);
+	}
+
+}
